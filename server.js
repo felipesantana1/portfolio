af9b018085aa83df756b971b49f50941c00b73dc
Felipe Santana <felipeo510.fs@gmail.com>
2018-08-09 00:39:31 -0700

    additional bugs fixed

--- server.js
+++ server.js
@@ -13,45 +13,48 @@ app.use(bodyParser.json());
 
 app.post("/email", (req, res)=>{
     if(req.body){
-        
-        var transporter = nodemailer.createTransport({
-            host: 'smtp.gmail.com',
-            port: 465,
-            secure: true,
-            auth: {
-                type: 'OAuth2',
-                user: process.env.GMAIL_USER,
-                pass: process.env.GAMIL_PASS,
-                clientId: process.env.CLIENT_ID,
-                clientSecret: process.env.CLIENT_SECRET,
-                accessToken: 'ya29.GlvzBUNHDuGspNG4gYrdU2TkB8l4R0MceOa0W2D6YAxAej8cqfmQxTnkElQb_jXEIbZCysXWYLvbE4pl-J0Ejh29_uqnC9uo54Q303O_K1pmz3N66MtU9ylFCGvf',
-                refreshToken: '1/PcvMrQFfsO9b2ETQVs174bNKW8BnbqLvIbcYhoX10lM'
-            }
-        });
-        transporter.set('oauth2_provision_cb', (user, renew, callback)=>{
-            let accessToken = userTokens[user];
-            if(!accessToken){
-                return callback(new Error('Unknown user'));
-            }else{
-                return callback(null, accessToken);
-            }
-        });
-        var mialOptions = {
-            from: req.body.email,
-            to: process.env.GMAIL_USER,
-            name: req.body.name,
-            subject: "portfolio message",
-            text: req.body.message
-        };
+        console.log(req.body);
+        res.redirect('/')
+        // var transporter = nodemailer.createTransport({
+        //     host: 'smtp.gmail.com',
+        //     port: 465,
+        //     secure: true,
+        //     auth: {
+        //         type: 'OAuth2',
+        //         user: process.env.GMAIL_USER,
+        //         pass: process.env.GAMIL_PASS,
+        //         clientId: process.env.CLIENT_ID,
+        //         clientSecret: process.env.CLIENT_SECRET,
+        //         refreshToken: '1/en-GL3PTL4rxUZTXWGFb7gGFDWyJkimGiwgUDvBRzaOEzIigUXjCqqIKYae6BBxD',
+        //         accessToken: 'ya29.Glv0BUih66I-zXu0d0RaFQM0LB9t5nAwovasV09etBeTTluNHnZAKrFfcIwD_BAmZV5bSvoTOy7nuZ_UFy58OOglF8EKOjwwuuATPB58A7ZXGNNDqgGrg7ZZ-FP-',
+        //         accessUrl: 'https://accounts.google.com/o/oauth2/v2/auth?redirect_uri=https://developers.google.com/oauthplayground&prompt=consent&response_type=code&client_id=407408718192.apps.googleusercontent.com&scope=https://mail.google.com/&access_type=offline'
 
-        transporter.sendMail(mialOptions, (err, info) => {
-            if(err){
-                console.log(err);
-                res.redirect("/")
-            } else {
-                res.redirect("/");
-            };
-        });
+        //     }
+        // });
+        // transporter.set('oauth2_provision_cb', (user, renew, callback)=>{
+        //     let accessToken = userTokens[user];
+        //     if(!accessToken){
+        //         return callback(new Error('Unknown user'));
+        //     }else{
+        //         return callback(null, accessToken);
+        //     }
+        // });
+        // var mialOptions = {
+        //     from: req.body.email,
+        //     to: process.env.GMAIL_USER,
+        //     name: req.body.name,
+        //     subject: "portfolio message",
+        //     text: req.body.message
+        // };
+
+        // transporter.sendMail(mialOptions, (err, info) => {
+        //     if(err){
+        //         console.log(err);
+        //         res.redirect("/")
+        //     } else {
+        //         res.redirect("/");
+        //     };
+        // });
     };
 });
 
