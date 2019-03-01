a2fc97ac96b28ce97d936bab074106e7183fc386
Felipe Santana <felipeo510.fs@gmail.com>
2019-03-01 09:48:33 -0800

    nav bar added with custom css and jquery

--- public/static/js/index.js
+++ public/static/js/index.js
@@ -1,20 +1,5 @@
-function pageScroll1(){
-    window.scrollTo(0, 800);
-    console.log("Working");
-}
-
-function pageScroll2(){
-    window.scrollTo(0, 1600);
-}
-
-function pageScroll3(){
-    window.scrollTo(0, 2700);
-}
-
-function pageScroll4(){
-    window.scrollTo(0, 3850);
-}
-
-function pageScroll5(){
-    window.scrollTo(0, 5000);
-}
+$(document).ready(function(){
+    $('.burger').click(function(){
+        $('.navlist').toggleClass('active')
+    })
+})
